4867c548657916316af510bbb5e4f8791f88a0c5
user <user@DESKTOP-8R6IVLS>
2022-02-03 13:02:33 +0530

    First Commit

--- JdbcProject1/src/Database/ClientTest.java
+++ JdbcProject1/src/Database/ClientTest.java
@@ -1,39 +1,40 @@
 package Database;
+
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-
-
 public class ClientTest {
 	public static void main(String[] args) throws SQLException {
 
 		Connection connection = DBUtil1.getConnection();
-		if(connection != null)
+		if (connection != null)
 			System.out.println("JDBC:connection is taken..");
-		Statement st=connection.createStatement();
-		String sql="select * from employee_table";
-		ResultSet rs=st.executeQuery(sql);
-		
-		while(rs.next())
-		{
-			 System.out.println(rs.getString(1));
-			
-			 
-				
-	}
-			try
-			{
-				connection.close();
-				st.close();
-			}
-			catch(Exception e)
-			{
-	    	   System.out.println(e);
-			}
+		Statement st = connection.createStatement();//type1 statement 
+		String sql = "select * from employee_table";
+
+		ResultSet rs = st.executeQuery(sql);
+
+		while (rs.next()) {
+			System.out.println(rs.getString(1));
+			System.out.println(rs.getString(2));
+			System.out.println(rs.getString(3));
+			System.out.println(rs.getInt(4));
+
 		}
 
+		/*
+		 * String sql="insert into employee_table(employee_id," +
+		 * "employee_name,email,esalary,bonus,doj) values(6,'vinay','vinay@gmail.com',40000,3000,'2022-03-23')"
+		 * ; int n=st.executeUpdate(sql); System.out.println(n + "row" + "afftected");
+		 */
+		try {
+			connection.close();
+			st.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
 
 }
-
